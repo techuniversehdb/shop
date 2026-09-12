937eb8c486765e974bbce4116fae226db45af61f
techuniversehdb <techuniversehdb@gmail.com>
2026-09-12 15:29:46 +0530

    Add files via upload

---
+++ social-config.js
@@ -0,0 +1,6 @@
+// ===============================
+// Google Apps Script Web App URL for Social (Tweets, Instagram, Reviews)
+// Replace with YOUR deployed web app URL (see social-apps-script-code.gs)
+// ===============================
+
+const SOCIAL_API_URL = "PASTE_YOUR_DEPLOYED_SOCIAL_WEB_APP_URL_HERE";
