2c704d9de35cd5e7e5e9b707ee5641d2d069e256
techuniversehdb <techuniversehdb@gmail.com>
2026-09-12 15:30:11 +0530

    Update SOCIAL_API_URL to deployed web app URL

--- social-config.js
+++ social-config.js
@@ -3,4 +3,4 @@
 // Replace with YOUR deployed web app URL (see social-apps-script-code.gs)
 // ===============================
 
-const SOCIAL_API_URL = "PASTE_YOUR_DEPLOYED_SOCIAL_WEB_APP_URL_HERE";
+const SOCIAL_API_URL = "https://script.google.com/macros/s/AKfycbxtHoxWOdOkns79ZNbxNFvr8x7wyczfMT0Tw8FI0kRy_gOQ_C9OeGJg6d6egDniL23kzA/exec";
